391dab7b807595baa8ca0106abf3d970b5a16637
Marco Di Marzo <marcodimarzo4@gmail.com>
2018-04-10 21:54:30 +0200

    HomeController

--- Java/src/main/java/client/controllers/HomeController.java
+++ Java/src/main/java/client/controllers/HomeController.java
@@ -2,64 +2,34 @@ package main.java.client.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HomeController {
+public class HomeController implements Initializable{
 
-    private Stage actual;
-    @FXML private Button anagraphic_b;
-    @FXML private Button canteen_b;
-    @FXML private Button trip_b;
-    @FXML private ImageView login;
+    @FXML private BorderPane borderPane;
+    @FXML private Button buttonAnagraphic;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Login button
+        buttonAnagraphic.setOnAction(event -> goToAnagraphic());
+    }
 
-    public void anagraphic() {
+    public void goToAnagraphic(){
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/anagraphic.fxml"));
-            actual = (Stage) anagraphic_b.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
+            Pane newPane = FXMLLoader.load(getClass().getResource("/views/addPerson.fxml"));
+            borderPane.setCenter(newPane);
         } catch (IOException e) {
             System.out.println("Errore nell'entrare in anagrafica");
         }
-    }
 
-    public void canteen() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/canteen.fxml"));
-            actual = (Stage) canteen_b.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nell'entrare in mensa");
-        }
-    }
-
-    public void trip() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/trips.fxml"));
-            actual = (Stage) trip_b.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nell'entrare in gite");
-        }
-    }
-
-    public void returnToLogin(){
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/LoginController.fxml"));
-            actual = (Stage) login.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nel tornare al LoginController");
-        }
     }
 }
