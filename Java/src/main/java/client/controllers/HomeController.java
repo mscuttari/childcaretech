36c15f7cf5431cb5f75bc2304ac8730dda8cef10
Marco Di Marzo <marcodimarzo4@gmail.com>
2018-04-10 22:21:24 +0200

    addPersonController

--- Java/src/main/java/client/controllers/HomeController.java
+++ Java/src/main/java/client/controllers/HomeController.java
@@ -3,24 +3,35 @@ package main.java.client.controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-
-
+import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable{
 
+    private Stage stage;
     @FXML private BorderPane borderPane;
     @FXML private Button buttonAnagraphic;
+    @FXML private ImageView goToLoginImage;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Login button
         buttonAnagraphic.setOnAction(event -> goToAnagraphic());
+        goToLoginImage.setOnMouseClicked(event -> {
+            try {
+                goToLogin();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     public void goToAnagraphic(){
@@ -32,4 +43,12 @@ public class HomeController implements Initializable{
         }
 
     }
+
+    public void goToLogin() throws  Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/views/login.fxml"));
+        stage = (Stage) goToLoginImage.getScene().getWindow();
+        stage.setScene(new Scene(root, 1000, 630));
+        stage.show();
+    }
+
 }
--- Java/src/main/java/client/controllers/addPersonController.java
+++ Java/src/main/java/client/controllers/addPersonController.java
@@ -1,78 +1,104 @@
 package main.java.client.controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class addPersonController {
+public class addPersonController implements Initializable {
 
-    @FXML private MenuButton personChoice;
-    @FXML private ImageView returnBack;
+    @FXML
+    private MenuButton personChoice;
     @FXML private StackPane stackPane;
-    @FXML private AnchorPane childrenPane;
-    @FXML private AnchorPane parentsPane;
-    @FXML private AnchorPane responsiblePane;
-    @FXML private AnchorPane contactsPane;
-    @FXML private AnchorPane pediatristPane;
+    @FXML private ImageView children;
+    @FXML private ImageView parents;
+    @FXML private ImageView responsible;
+    @FXML private ImageView contacts;
+    @FXML private ImageView pediatrist;
+    @FXML private Tab tabParents;
+    @FXML private Tab tabPediatrist;
+    @FXML private Tab tabAllergies;
+    @FXML private Tab tabIntollerances;
+    @FXML private Tab tabContacts;
+    @FXML private Tab tabLoginData;
+    @FXML private TabPane tabPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        personChoice.setText("Bambino");
+
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
+        tabPane.getTabs().addAll(tabParents, tabPediatrist, tabAllergies, tabIntollerances, tabContacts);
+    }
 
     public void childrenChoice() {
         for (Node current : stackPane.getChildren()) {
             current.setVisible(false);
         }
-        childrenPane.setVisible(true);
+        children.setVisible(true);
         personChoice.setText("Bambino");
+
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
+        tabPane.getTabs().addAll(tabParents, tabPediatrist, tabAllergies, tabIntollerances, tabContacts);
+
     }
 
     public void parentsChoice(){
         for (Node current : stackPane.getChildren()) {
-                current.setVisible(false);
-            }
-        parentsPane.setVisible(true);
+            current.setVisible(false);
+        }
+        parents.setVisible(true);
         personChoice.setText("Genitore");
+
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
     }
 
     public void responsibleChoice(){
         for (Node current : stackPane.getChildren()) {
             current.setVisible(false);
         }
-        responsiblePane.setVisible(true);
+        responsible.setVisible(true);
         personChoice.setText("Personale");
+
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
+        tabPane.getTabs().addAll(tabAllergies, tabIntollerances, tabContacts, tabLoginData);
+
     }
 
     public void contactsChoice(){
         for (Node current : stackPane.getChildren()) {
             current.setVisible(false);
         }
-        contactsPane.setVisible(true);
+        contacts.setVisible(true);
         personChoice.setText("Contatto");
+
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
+
     }
 
     public void pediatristChoice(){
         for (Node current : stackPane.getChildren()) {
             current.setVisible(false);
         }
-        pediatristPane.setVisible(true);
+        pediatrist.setVisible(true);
         personChoice.setText("Pediatra");
-    }
 
-    public void returnBack() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/anagraphic.fxml"));
-            Stage actual = (Stage) returnBack.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nel tornare indietro");
-        }
-    }
+        int size = tabPane.getTabs().size();
+        tabPane.getTabs().remove(1, size);
+        tabPane.getTabs().addAll(tabAllergies, tabIntollerances);
 
+    }
 }
