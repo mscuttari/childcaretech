c19fde92823146c6849d75876ea56cd641f172cd
Marco Di Marzo <marcodimarzo4@gmail.com>
2018-04-13 11:55:02 +0200

    Add ParentsTableView in AddPersonController

--- Java/src/main/java/client/controllers/AddPersonController.java
+++ Java/src/main/java/client/controllers/AddPersonController.java
@@ -1,13 +1,18 @@
 package main.java.client.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import main.java.client.ParentTable;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,12 +20,15 @@ import java.util.ResourceBundle;
 public class AddPersonController implements Initializable {
 
     @FXML private MenuButton personType;
+    @FXML private Pane parentsPane;
+
     @FXML private StackPane stackPane;
     @FXML private ImageView children;
     @FXML private ImageView parents;
     @FXML private ImageView responsible;
     @FXML private ImageView contacts;
     @FXML private ImageView pediatrist;
+
     @FXML private Tab tabParents;
     @FXML private Tab tabPediatrist;
     @FXML private Tab tabAllergies;
@@ -29,14 +37,54 @@ public class AddPersonController implements Initializable {
     @FXML private Tab tabLoginData;
     @FXML private TabPane tabPane;
 
+    //@FXML private VBox parentsBox;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        personType.setText("Bambino");
+        tabPane.getTabs().remove(tabLoginData);
 
-        int size = tabPane.getTabs().size();
-        tabPane.getTabs().remove(1, size);
-        tabPane.getTabs().addAll(tabParents, tabPediatrist, tabAllergies, tabIntollerances, tabContacts);
+        /*
+        // Connection
+        ConnectionManager connectionManager = ConnectionManager.getInstance();
+
+        //Parents tab
+        List<Parent> parents = connectionManager.getClient().getParents();
+        for(Parent current : parents){
+            CheckBox checkBox = new CheckBox(current.toString());
+            parentsBox.getChildren().add(checkBox);
+        }
+        */
+
+        ObservableList<ParentTable> parents = FXCollections.observableArrayList();
+
+        parents.add( new ParentTable( "Mario", "Rossi", false));
+        parents.add( new ParentTable( "Maria", "Neri", false));
+        parents.add( new ParentTable( "Luca", "Grigi", false));
+        for (int i = 0; i < 100; i++) parents.add(new ParentTable("nome"+(i+3), "Cognome"+(i+3), false));
+
+        TableView<ParentTable> table = new TableView<ParentTable>();
+        table.setEditable(true);
+
+
+        TableColumn<ParentTable,Boolean> c1 = new TableColumn<ParentTable,Boolean>("Genitore");
+        c1.setCellValueFactory(new PropertyValueFactory<ParentTable,Boolean>("check"));
+        c1.setCellFactory(column -> new CheckBoxTableCell());
+        table.getColumns().add(c1);
+
+        TableColumn<ParentTable,String> c2 = new TableColumn<ParentTable,String>("Nome");
+        c2.setCellValueFactory(new PropertyValueFactory<ParentTable,String>("name"));
+        table.getColumns().add(c2);
+
+        TableColumn<ParentTable,String> c3 = new TableColumn<ParentTable,String>("Cognome");
+        c3.setCellValueFactory(new PropertyValueFactory<ParentTable,String>("surname"));
+        table.getColumns().add(c3);
+
+        table.setItems(parents);
+
+        parentsPane.getChildren().addAll(table);
+        table.prefWidthProperty().bind(parentsPane.widthProperty());
+        table.prefHeightProperty().bind(parentsPane.heightProperty());
     }
 
     public void childrenChoice() {
@@ -49,7 +97,6 @@ public class AddPersonController implements Initializable {
         int size = tabPane.getTabs().size();
         tabPane.getTabs().remove(1, size);
         tabPane.getTabs().addAll(tabParents, tabPediatrist, tabAllergies, tabIntollerances, tabContacts);
-
     }
 
     public void parentsChoice(){
