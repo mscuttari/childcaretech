6c40c57336cb7dcc2d082a76828e854f56494f77
Marco Di Marzo <marcodimarzo4@gmail.com>
2018-04-14 13:22:22 +0200

    MenuController

--- Java/src/main/java/client/controllers/MenuController.java
+++ Java/src/main/java/client/controllers/MenuController.java
@@ -1,52 +1,39 @@
 package main.java.client.controllers;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import java.io.IOException;
+import javafx.scene.layout.Pane;
 
-public class MenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    private Stage actual;
-    @FXML private ImageView canteen;
-    @FXML private Button showM_b;
-    @FXML private Button addM_b;
+public class MenuController implements Initializable{
 
-    public void showMenu() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/showMenu.fxml"));
-            actual = (Stage) showM_b.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nell'entrare in 'visualizza persone'");
-        }
+    // Debug
+    private static final String TAG = "MenuController";
+
+    @FXML private Pane menuPane;
+    @FXML private Button buttonAddMenu;
+    @FXML private Button buttonUpdateMenu;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        // Add menu button
+        buttonAddMenu.setOnAction(event -> addMenu());
+
+        //Update menu button
+        buttonUpdateMenu.setOnAction(event -> updateMenu());
     }
 
     public void addMenu() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/addMenu.fxml"));
-            actual = (Stage) addM_b.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nell'entrare in 'aggiungi persone'");
-        }
+
     }
 
-    public void returnToCanteen() {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("/views/CanteenController.fxml"));
-            Stage actual = (Stage) canteen.getScene().getWindow();
-            actual.setScene(new Scene(root, 1000, 630));
-            actual.show();
-        } catch (IOException e) {
-            System.out.println("Errore nel tornare alla HomeController");
-        }
+    public void updateMenu() {
+
     }
+
 }
