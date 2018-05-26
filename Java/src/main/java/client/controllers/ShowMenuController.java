0d7180c8385734db5fae25608316040395137cf5
Marco Di Marzo <marcodimarzo4@gmail.com>
2018-05-26 19:09:29 +0200

    Show Menu Controller

--- Java/src/main/java/client/controllers/ShowMenuController.java
+++ Java/src/main/java/client/controllers/ShowMenuController.java
@@ -14,14 +14,16 @@ import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 import main.java.LogUtils;
 import main.java.client.connection.ConnectionManager;
+import main.java.client.gui.GuiMenu;
 import main.java.client.layout.MyButtonTableCell;
+import main.java.client.utils.TableUtils;
 import main.java.models.Menu;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
-public class ShowMenuController implements Initializable{
+public class ShowMenuController extends AbstractController implements Initializable {
 
     // Debug
     private static final String TAG = "ShowMenuController";
@@ -29,47 +31,46 @@ public class ShowMenuController implements Initializable{
     @FXML private Pane showMenuPane;
     @FXML private ImageView goBackImage;
 
-    @FXML private TableView<Menu> tableMenu;
-    @FXML private TableColumn<Menu, String> columnMenuName;
-    @FXML private TableColumn<Menu, String> columnMenuDayOfTheWeek;
-    @FXML private TableColumn<Menu, Void> columnMenuEdit;
-    @FXML private TableColumn<Menu, Void> columnMenuShowDetails;
-    @FXML private TableColumn<Menu, Void> columnMenuDelete;
+    @FXML private TableView<GuiMenu> tableMenu;
+    @FXML private TableColumn<GuiMenu, String> columnMenuName;
+    @FXML private TableColumn<GuiMenu, String> columnMenuDayOfTheWeek;
+    @FXML private TableColumn<GuiMenu, Void> columnMenuEdit;
+    @FXML private TableColumn<GuiMenu, Void> columnMenuShowDetails;
+    @FXML private TableColumn<GuiMenu, Void> columnMenuDelete;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // go back button
+        goBackImage.setOnMouseEntered(event -> showMenuPane.getScene().setCursor(Cursor.HAND));
+        goBackImage.setOnMouseExited(event -> showMenuPane.getScene().setCursor(Cursor.DEFAULT));
+        goBackImage.setOnMouseClicked(event -> goBack());
+
+
         // Connection
         ConnectionManager connectionManager = ConnectionManager.getInstance();
 
-        // go back button cursor
-        goBackImage.setOnMouseEntered(event -> showMenuPane.getScene().setCursor(Cursor.HAND));
-        goBackImage.setOnMouseExited(event -> showMenuPane.getScene().setCursor(Cursor.DEFAULT));
-
-        //go back image
-        goBackImage.setOnMouseClicked(event -> goBack());
-
         // Table
         List<Menu> menus = connectionManager.getClient().getMenus();
-        ObservableList<Menu> menusData = FXCollections.observableArrayList(menus);
+        @SuppressWarnings("unchecked") ObservableList<GuiMenu> menusData = TableUtils.getGuiModelsList(menus);
 
         columnMenuName.setCellValueFactory(new PropertyValueFactory<>("name"));
         columnMenuDayOfTheWeek.setCellValueFactory(new PropertyValueFactory<>("dayOfTheWeek"));
 
-        columnMenuEdit.setCellFactory(param -> new MyButtonTableCell<>("Modifica", new Callback<Menu, Object>() {
+
+        columnMenuEdit.setCellFactory(param -> new MyButtonTableCell<>("Modifica", new Callback<GuiMenu, Object>() {
 
             @Override
-            public Object call(Menu param) {
+            public Object call(GuiMenu param) {
                 try {
-
                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/updateMenu.fxml"));
 
-                    UpdateMenuController updateMenuController = new UpdateMenuController(param);
-                    loader.setController(updateMenuController);
-
                     Pane updateMenuPane = loader.load();
-                    BorderPane homePane = (BorderPane) showMenuPane.getParent();
+                    UpdateMenuController controller = loader.getController();
+                    controller.setMenu(param.getModel());
+
+                    BorderPane homePane = (BorderPane)showMenuPane.getParent();
                     homePane.setCenter(updateMenuPane);
 
                 } catch (IOException e) {
@@ -79,19 +80,18 @@ public class ShowMenuController implements Initializable{
                 return null;
             }
         }));
-        columnMenuShowDetails.setCellFactory(param -> new MyButtonTableCell<>("Visualizza dettagli", new Callback<Menu, Object>() {
+        columnMenuShowDetails.setCellFactory(param -> new MyButtonTableCell<>("Visualizza dettagli", new Callback<GuiMenu, Object>() {
 
             @Override
-            public Object call(Menu param) {
+            public Object call(GuiMenu param) {
                 try {
-
                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/showMenuDetails.fxml"));
 
-                    ShowMenuDetailsController showMenuDetailsController = new ShowMenuDetailsController(param);
-                    loader.setController(showMenuDetailsController);
-
                     Pane showMenuDetailsPane = loader.load();
-                    BorderPane homePane = (BorderPane) showMenuPane.getParent();
+                    ShowMenuDetailsController controller = loader.getController();
+                    controller.setMenu(param.getModel());
+
+                    BorderPane homePane = (BorderPane)showMenuPane.getParent();
                     homePane.setCenter(showMenuDetailsPane);
 
                 } catch (IOException e) {
@@ -102,16 +102,19 @@ public class ShowMenuController implements Initializable{
             }
         }));
         columnMenuDelete.setCellFactory(param -> new MyButtonTableCell<>("Elimina", param1 -> {
+            if (param1.getModel().isDeletable()) {
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Vuoi davvero eliminare il menù?\n" +
+                        "(la procedura è irreversibile)", ButtonType.NO, ButtonType.YES);
+                alert.setTitle("Conferma operazione");
+                alert.setHeaderText(null);
 
-            //delete
-            connectionManager.getClient().delete(param1);
 
-            try {
-                Pane newPaneShowMenu = FXMLLoader.load(getClass().getResource("/views/showMenu.fxml"));
-                BorderPane homePane = (BorderPane) showMenuPane.getParent();
-                homePane.setCenter(newPaneShowMenu);
-            } catch (IOException e) {
-                LogUtils.e(TAG, e.getMessage());
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.YES) {
+                    deleteData(connectionManager, param1);
+                }
+            } else {
+                showErrorDialog("Questo menù non può essere eliminato");
             }
 
             return null;
@@ -121,14 +124,22 @@ public class ShowMenuController implements Initializable{
         tableMenu.setItems(menusData);
     }
 
-    public void goBack() {
-        try {
-            Pane menuPane = FXMLLoader.load(getClass().getResource("/views/menu.fxml"));
-            BorderPane homePane = (BorderPane) showMenuPane.getParent();
-            homePane.setCenter(menuPane);
-        } catch (IOException e) {
-            LogUtils.e(TAG, e.getMessage());
-        }
+    /**
+     * Delete data and update table data
+     */
+    private void deleteData(ConnectionManager connectionManager, GuiMenu param) {
+        connectionManager.getClient().delete(param.getModel());
+        List<Menu> newMenus = connectionManager.getClient().getMenus();
+        @SuppressWarnings("unchecked") ObservableList<GuiMenu> newMenusData = TableUtils.getGuiModelsList(newMenus);
+
+        tableMenu.setItems(newMenusData);
+    }
+
+    /**
+     * Go back to the dish page
+     */
+    private void goBack() {
+        setCenterFXML((BorderPane)showMenuPane.getParent(), "/views/menu.fxml");
     }
 
 }
